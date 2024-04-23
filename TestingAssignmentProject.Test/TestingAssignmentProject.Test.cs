acf00287f456354751bc90d91c600b90583c7d8b
Roisin-m <S00223865@atu.ie>
2024-04-23 17:54:17 +0100

    TestCases added to test method

--- TestingAssignmentProject.Test/TestingAssignmentProject.Test.cs
+++ TestingAssignmentProject.Test/TestingAssignmentProject.Test.cs
@@ -8,19 +8,28 @@ namespace TestingAssignmentProject.Test
     public class InsuranceServiceTest
     {
 
+        [TestCase(19, "rural", 5.0)]
+        [TestCase(32, "rural", 2.5)]
+        [TestCase(15, "rural", 0.0)]
+        [TestCase(19, "urban", 6.0)]
+        [TestCase(37, "urban", 5.0)]
+        [TestCase(15, "urban", 0.0)]
+        [TestCase(51, "suburban", 0.0)]
+        [TestCase(51, "urban", 2.5)]
+
         [Test]
-        public void CalculatePremium_WhenRural_BetweenAge18and30()
+        public void CalculatePremium_WhenLocationAndAgeEntered(int age, string location, double expectedPremium)
         {
             // Arrange
             var mockDiscountService = new Mock<IDiscountService>();
-            mockDiscountService.Setup(x => x.GetDiscount()).Returns(1.0);  // No discount
+            mockDiscountService.Setup(x => x.GetDiscount()).Returns(0.5);  //set discount at 50%
             var insuranceService = new InsuranceService(mockDiscountService.Object);
 
             // Act
-            double premium = insuranceService.CalcPremium(25, "rural");
+            double premium = insuranceService.CalcPremium(age, location);
 
             // Assert
-            Assert.That(premium, Is.EqualTo(5.0));  // Expected premium is 5.0 for rural location and age between 18 and 30
+            Assert.That(premium, Is.EqualTo(expectedPremium)); 
         }
     }
 }
